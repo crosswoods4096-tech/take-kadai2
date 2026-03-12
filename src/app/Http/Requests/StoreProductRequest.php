dae5ecb3dd359d9becc05069560f2b87806fa8bc
takepon <crosswoods4096@yahoo.co.jp>
2026-03-12 17:29:10 +0900

    バリデーションメッセージを変更

--- src/app/Http/Requests/StoreProductRequest.php
+++ src/app/Http/Requests/StoreProductRequest.php
@@ -26,17 +26,17 @@ class StoreProductRequest extends FormRequest
     public function messages()
     {
         return [
-            'name.required'        => '商品名は必須です。',
+            'name.required'        => '商品名を入力してください。',
             'price.required'       => '値段を入力してください。',
-            'price.integer'        => '値段は数値で入力してください。',
-            'price.min'            => '値段は0円以上で入力してください。',
-            'price.max'            => '値段は10000円以内で入力してください。',
-            'image.required'       => '商品画像は必須です。',
+            'price.integer'        => '数値で入力してください。',
+            'price.min'            => '0～10000円以内で入力してください。',
+            'price.max'            => '0～10000円以内で入力してください。',
+            'image.required'       => '画像を登録してください。',
             'image.image'          => '画像ファイルを選択してください。',
-            'image.mimes'          => '画像はjpegまたはpng形式のみ対応しています。',
-            'seasons.required'     => '季節は必ず選択してください。',
-            'description.required' => '商品説明は必須です。',
-            'description.max'      => '商品説明は120文字以内で入力してください。',
+            'image.mimes'          => '「.png」または「.jpeg」形式でアップロードしてください。',
+            'seasons.required'     => '季節を選択してください。',
+            'description.required' => '商品説明を入力してください。',
+            'description.max'      => '120文字以内で入力してください。',
         ];
     }
 }
--- src/app/Http/Requests/UpdateProductRequest.php
+++ src/app/Http/Requests/UpdateProductRequest.php
@@ -26,15 +26,15 @@ class UpdateProductRequest extends FormRequest
     public function messages()
     {
         return [
-            'name.required'        => '商品名は必須です。',
-            'price.required'       => '値段は必須です。',
-            'price.integer'        => '値段は数値で入力してください。',
-            'price.max'            => '値段は10000円以内で入力してください。',
-            'image.image'          => '商品画像は画像ファイルを選択してください。',
-            'image.mimes'          => '商品画像はjpegまたはpng形式でアップロードしてください。',
-            'seasons.required'     => '季節は1つ以上選択してください。',
-            'description.required' => '商品説明は必須です。',
-            'description.max'      => '商品説明は120文字以内で入力してください。',
+            'name.required'        => '商品名を入力してください。',
+            'price.required'       => '値段を入力してください。',
+            'price.integer'        => '数値で入力してください。',
+            'price.max'            => '0～10000円以内で入力してください。',
+            'image.image'          => '画像を登録してください。',
+            'image.mimes'          => '「.png」または「.jpeg」形式でアップロードしてください。',
+            'seasons.required'     => '季節を選択してください。',
+            'description.required' => '商品説明を入力してください。',
+            'description.max'      => '120文字以内で入力してください。',
         ];
     }
 }
